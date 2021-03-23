faf99311c946c121324119c13842bbfd2626c632
Kote-ko <79904428+Kote-ko@users.noreply.github.com>
2021-03-23 14:56:14 +0400

    update script js

--- js/script.js
+++ js/script.js
@@ -11,3 +11,53 @@ $("#wrapper").click( function() {
   $(".icon").toggleClass("close");
 });
 
+
+
+$(function () {
+	$('.multiple-items').slick({
+  		 slidesToShow: 4,
+  		 slidesToScroll: 1,
+  		 arrows: true,
+  		 autoplay: true,
+  		 autoplaySpeed: 100000,
+  		 speed: 2500,
+  		 dots: false,
+  		 prevArrow: ".arrow-prev",
+  		 nextArrow: ".arrow-next",
+  		 responsive: [
+    {
+      breakpoint: 1040,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+	});
+});
+
+
+// $('.item').mousemove(function(e) {
+// 	$(this).children('a').children('img').animate({ height: '250', width: '350'} 100);
+// }).mouseleave(function(e) {
+// 	$(this).children('a').children('img').animate({ height: '90%', width: '90%'} 100);
+// });
