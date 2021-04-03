05a78c7363de731d3b0a4585cd1c689d1a4af14d
Kote-ko <79904428+Kote-ko@users.noreply.github.com>
2021-04-03 14:02:48 +0400

    update script js

--- js/script.js
+++ js/script.js
@@ -65,17 +65,7 @@ $(function () {
 // slick-slider (carousel)
 
 
-/* animation text opacity*/
-// var wrapper = document.getElementsByClassName("text-left-1")[0];
-// wrapper.style.opacity="1";
-// wrapper.innerHTML = wrapper.textContent.replace(/./g,"<span>$&</span>");
 
-// var spans = wrapper.getElementsByTagName("span");
-
-// for(var i=0;i<spans.length;i++){
-//   spans[i].style.animationDelay = i*80+"ms";
-// } 
-/* animation text opacity*/
 
 
 
