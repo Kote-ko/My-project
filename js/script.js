c9a67d334424635578b900f44b15bd5bacd9bbba
Kote-ko <79904428+Kote-ko@users.noreply.github.com>
2021-04-03 13:48:16 +0400

    update script js

--- js/script.js
+++ js/script.js
@@ -48,6 +48,13 @@ $(function () {
         slidesToShow: 1,
         slidesToScroll: 1
       }
+    },
+     {
+      breakpoint: 360,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
     }
     // You can unslick at a given breakpoint now by adding:
     // settings: "unslick"
